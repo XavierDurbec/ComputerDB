a73994513a1b0fefac4665d6a16a030667eeacdc
XavierDurbec <xavier-durbec@etud.univ-tln.fr>
2018-04-17 12:15:28 +0200

    [REFACTO] (pom) transfert all version in properties

--- webapp/src/main/java/com/excilys/xdurbec/formation/computerdatabase/springconfig/SecurityWebApplicationInitializer.java
+++
@@ -1,8 +0,0 @@
-package com.excilys.xdurbec.formation.computerdatabase.springconfig;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer 
-  extends AbstractSecurityWebApplicationInitializer {
-
-}
